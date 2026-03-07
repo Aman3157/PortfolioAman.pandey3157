6c20faed96c5816f405d5feaa66e0d0c34c83b4c
Aman Pandey <pandeyaman3157@gmail.com>
2026-03-07 23:09:36 +0530

    footer changes

--- src/component/footer.js
+++ src/component/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer style={{background:"#222", color:"white", padding:"10px", textAlign:"center",top:'0'}}>
-      <p>© 2026 My Website | All Rights Reserved</p>
+      <p>© 2026 Aman Profile | All Rights Reserved</p>
     </footer>
   );
 }
